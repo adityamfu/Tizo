6af9c794d91802b711b074455d0087bf650204c6
adit <akunpengganti.aditya@gmail.com>
2025-10-24 22:34:49 +0700

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import FloatingCredit from "./components/FloatingCredit";
 
 function App() {
   return (
-    <div className="bg-background text-foreground">
+    <div className="bg-background text-foreground min-h-screen">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
-        {/* jika route tidak cocok, redirect ke home */}
+        {/* Jika route tidak cocok, redirect ke home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <FloatingCredit />
